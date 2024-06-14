da4af7302b4acc4ce6333f064b40ee031dea497a
Ahmad Didik Setiyadi <adidiksetiyadi@gmail.com>
2024-06-14 22:59:50 +0700

    solusi esp32-flask

--- sic_2024/flask/basic.py
+++ sic_2024/flask/basic.py
@@ -1,29 +1,25 @@
-#!/usr/bin/python
-# coding: utf-8
-from flask import Flask
-from flask import request
+from flask import Flask, request
 
 app = Flask(__name__)
 
 data = []
 
-@app.route('/sic')
-def sic():
-    return 'Kelas SIC 2024'
-
 @app.route('/sensor/data', methods=['POST'])
 def post():
-    temperature = request.args.get("temperature")
-    kelembapan = request.args.get("kelembapan")
-    subData = [temperature, kelembapan]
-    
-    data.append(subData)
-
-    return  'berhasil disimpan ke server'
+    temperature = request.form.get("temperature")
+    kelembapan = request.form.get("kelembapan")
+    print(f"Received temperature: {temperature}")
+    print(f"Received kelembapan: {kelembapan}")
+    if temperature is not None and kelembapan is not None:
+        subData = [temperature, kelembapan]
+        data.append(subData)
+        return 'berhasil disimpan ke server', 200
+    else:
+        return 'Failed to receive data', 400
 
 @app.route('/sensor/data', methods=['GET'])
 def get():
     return data
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
